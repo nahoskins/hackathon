29a4ced4abd50fefd838c61592ec8afd38d8afd6
nahoskins <nahoskins@gmail.com>
2014-12-08 04:23:20 +0000

    fucking work already2

--- app/js/services.js
+++ app/js/services.js
@@ -2,7 +2,7 @@ var eulaseesServices = angular.module('myApp.eulaseesServices',['ngResource']);
 
 eulaseesServices.factory('Eula', ['$resource',
   function($resource){
-    return $resource('swfiua.koding.io:8000/eulasees/everythingforeula/', {}, {
+    return $resource('swfiua.koding.io:8000/eulasees/everythingforeula/3', {}, {
       query: {method:'GET', params:{eulaId:'Eula'}, isArray:true}
     });
   }]);
